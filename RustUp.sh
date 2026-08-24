2e53e9e0167adbdf5552d9e324dd823f27cdf706
Harrison-Uhl <harrison.uhl@gmail.com>
2026-08-24 12:09:15 -0400

    Update RustUp.sh

--- RustUp.sh
+++ RustUp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # --- Usage:
-#export GitOpsUrl=https://raw.githubusercontent.com/Harrison-Uhl/BETA-boot-GitOps/master
-git clone --depth 1 --single-branch -b main $GitOpsUrl
-cat RustUp.sh
+# export GitOpsUrl=https://raw.githubusercontent.com/Harrison-Uhl/BETA-boot-GitOps/master
+# git clone --depth 1 --single-branch -b main $GitOpsUrl
+# cat RustUp.sh
+
 #xxx??  curl -fSL $WptlGit/cfSrvRootFSH -o $Srv/env/cfSrvRootFSH.env 
 
 # httpsRustUp.sh
