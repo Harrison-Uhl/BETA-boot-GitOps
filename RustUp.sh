a80d36ff226d8c682f3f0317ac5cab2a1081b898
Harrison-Uhl <harrison.uhl@gmail.com>
2026-09-01 17:28:39 -0400

    Update RustUp.sh

--- RustUp.sh
+++ RustUp.sh
@@ -9,3 +9,13 @@
 
 set +euxo
 
+# Add the following early to avoid warnings during RuspUP...
+sudo dnf install cmake gcc make curl clang llvm lld -y
+
+# 1. Initialize Rustup silently passing automatic defaults (-y)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# 2. Actively link environment maps onto your current active profile instance
+source "$HOME/.cargo/env"
+# 3. Verify total tool environment status flags
+echo '--- Versions ---'
+rustc --version && cargo --version
